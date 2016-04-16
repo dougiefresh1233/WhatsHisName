4456f084da504bb3b1bd8a8554e000b85380660e
dougiefresh1233 <douglas.cannon18@gmail.com>
2016-04-16 13:37:03 -0500

    Added (some) function to the Save Button

--- What'sHisName/app/src/main/java/com/example/dougl/whatshisname/AddActivity.java
+++ What'sHisName/app/src/main/java/com/example/dougl/whatshisname/AddActivity.java
@@ -3,11 +3,20 @@ package com.example.dougl.whatshisname;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
+
+    private String name;
+    private String description;
+    private EditText editName;
+    private EditText editDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,26 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        editName = (EditText) findViewById(R.id.editName);
+        editDescription = (EditText) findViewById(R.id.editDescription);
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = editName.getText().toString();
+                description = editDescription.getText().toString();
+                //Send stuff to server
+                //go back to main activity
+                goBack();
+            }
+        });
+    }
+
+    ///<summary>
+    ///Goes back to Main Activity
+    ///</summary>
+    public void goBack(){
+        NavUtils.navigateUpFromSameTask(this);
     }
 
 }
