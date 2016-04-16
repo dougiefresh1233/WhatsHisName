0c361b99064313740ffbb92aa5f7eabbd325bd51
Smarwell <smarewell@gmail.com>
2016-04-16 15:50:28 -0500

    Button linked to EditActivity

--- What'sHisName/app/src/main/java/com/example/dougl/whatshisname/ViewActivity.java
+++ What'sHisName/app/src/main/java/com/example/dougl/whatshisname/ViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.dougl.whatshisname;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,8 @@ public class ViewActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent openeditor = new Intent(ViewActivity.this,EditActivity.class);
+                startActivity(openeditor);
             }
         });
 
