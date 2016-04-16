9e18590bd9606a955d1c2da856acc34c29efb00e
dougiefresh1233 <douglas.cannon18@gmail.com>
2016-04-16 14:51:31 -0500

    Merged by hand

--- What'sHisName/app/src/main/java/com/example/dougl/whatshisname/ViewActivity.java
+++ What'sHisName/app/src/main/java/com/example/dougl/whatshisname/ViewActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -24,6 +26,11 @@ public class ViewActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        TextView name=(TextView) findViewById(R.id.nameView);
+        name.setText("Bitch ass niggas be trippin");
+
+
     }
 
 }
